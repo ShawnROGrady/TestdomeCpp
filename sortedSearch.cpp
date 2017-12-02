24085b9836966864b72903e4674dabbe2d05e7c2
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-02 13:45:01 -0600

    Add files via upload

---
+++ sortedSearch.cpp
@@ -0,0 +1,27 @@
+//
+//  sortedSearch.cpp
+//  sortedSearch
+//
+//  Created by Shawn O'Grady on 12/2/17.
+//  Copyright © 2017 Shawn O'Grady. All rights reserved.
+//
+
+/*
+ This code is a practice C++ interview question from testdome.com
+ https://www.testdome.com/for-developers/solve-question/11891
+ */
+
+#include <stdio.h>
+#include <vector>
+#include <stdexcept>
+#include <iostream>
+
+
+class SortedSearch
+{
+public:
+    static int countNumbers(const std::vector<int>& sortedVector, int lessThan)
+    {
+        throw std::logic_error("Waiting to be implemented");
+    }
+};
