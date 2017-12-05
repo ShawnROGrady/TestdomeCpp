f45eb3ca881e83f46cec7133ed9c5bde4b855310
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-05 16:37:08 -0600

    Add files via upload

---
+++ trainComposition.cpp
@@ -0,0 +1,36 @@
+//
+//  trainComposition.cpp
+//  trainComposition
+//
+//  Created by Shawn O'Grady on 12/5/17.
+//  Copyright © 2017 Shawn O'Grady. All rights reserved.
+//
+
+
+#include <stdexcept>
+#include <iostream>
+#include <stdio.h>
+
+class TrainComposition
+{
+public:
+    void attachWagonFromLeft(int wagonId)
+    {
+        throw std::logic_error("Waiting to be implemented");
+    }
+    
+    void attachWagonFromRight(int wagonId)
+    {
+        throw std::logic_error("Waiting to be implemented");
+    }
+    
+    int detachWagonFromLeft()
+    {
+        throw std::logic_error("Waiting to be implemented");
+    }
+    
+    int detachWagonFromRight()
+    {
+        throw std::logic_error("Waiting to be implemented");
+    }
+};
