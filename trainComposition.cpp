6754391524e267c446842aee91dad861c8b5ce52
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-05 16:41:16 -0600

    Added problem statement
    Added problem statement to comments at the beginning of the file

--- trainComposition.cpp
+++ trainComposition.cpp
@@ -6,6 +6,15 @@
 //  Copyright © 2017 Shawn O'Grady. All rights reserved.
 //
 
+/*
+ This code is a practice C++ interview question from testdome.com
+ https://www.testdome.com/for-developers/solve-question/11908
+ 
+ Problem statement: A TrainComposition is built by attaching and detaching wagons from the left and the right sides.
+    -For example, if we start by attaching wagon 7 from the left followed by attaching wagon 13, again from the left, we get a composition of two wagons (13 and 7 from left to right). 
+    -Now the first wagon that can be detached from the right is 7 and the first that can be detached from the left is 13.
+    -Implement a TrainComposition that models this problem
+ */
 
 #include <stdexcept>
 #include <iostream>
