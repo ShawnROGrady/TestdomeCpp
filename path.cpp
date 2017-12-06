af86db5f9a15e2ff93180ac6cc5b70fd0b1d229f
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-06 17:02:09 -0600

    Fixed direct pathname
    Fixed my case for a direct pathname, but I am still only getting 1/4 tests right on testdome.com. I also added some minor things to catch the cases where the user wishes to go to the root directory. I am really not sure what is missing.
    
    In the commented out main() function at the bottom of the file, I have included the test cases I performed. Each commented out line is supposed to be run individually in order to test various use cases. Each of these gave the expected output, so I am not sure what type of test cases they are running on testdome.com.

--- path.cpp
+++ path.cpp
@@ -20,6 +20,8 @@
         -do not use built-in path-related functions
  
  + References: https://en.wikipedia.org/wiki/Cd_(command) , http://linuxcommand.org/lc3_lts0020.php
+ 
+ + Only says its passing 1/4 tests, but I can't tell what I'm missing...
  */
 #include <stdio.h>
 #include <iostream>
@@ -43,56 +45,59 @@ public:
     {
         
         int newPathLength=newPath.length();
-        //std::cout<<"newPathLength="<<newPathLength;
-        int i=0;
-        while(i<newPathLength){
-            int pathLength=currentPath.length();
-            if(newPath[i]=='.'&&newPath[i+1]=='.'){
-                //move up one directory
-                if(pathLength>2){
-                    currentPath.erase(pathLength-2,2);
-                    i=i+3;
+        if(newPathLength>0&&newPath[0]!='~'){
+            int i=0;
+            while(i<newPathLength&&!isspace(newPath[i])){
+                int pathLength=currentPath.length();
+                if(newPath[i]=='.'&&newPath[i+1]=='.'){
+                    //move up one directory
+                    if(pathLength>2){
+                        currentPath.erase(pathLength-2,2);
+                        i=i+3;
+                    }
+                    else{
+                        //root directory
+                        currentPath.erase(pathLength-1,2);
+                        i=i+3;
+                    }
+                    
+                }
+                else if(newPath[i]=='/'){
+                    //direct pathname
+                    currentPath.erase(0,pathLength);
+                    int j=0;
+                    while(newPath[j]!='.'&&j<newPathLength){
+                        //this is ok since we are assuming no invalid paths will be given
+                        currentPath=currentPath+newPath[j];
+                        j++;
+                    }
+                    if(currentPath[j-1]=='/'){
+                        currentPath.erase(j-1,1);   //get rid of extra '/'
+                    }
+                    i=i+j;
+                }
+                else if(newPath[i]=='.'){
+                    if(newPath[i+1]=='/'){
+                        //relative pathname to child directory
+                        i=i+2;
+                    }
+                    else{
+                        i=i+1;
+                    }
+                    
                 }
                 else{
-                    //root directory
-                    currentPath.erase(pathLength-1,2);
-                    i=i+3;
-                }
-                
-            }
-            else if(newPath[i]=='/'){
-                //direct pathname
-                /*
-                int j=pathLength-1;
-                while(currentPath[j]!=newPath[i+1]){
-                    //this is ok since we are assuming no invalid paths will be given
-                    currentPath.erase(pathLength-1, 1);
-                    j=j-1;
-                }
-                
-                if(pathLength>2){
-                    currentPath.erase(pathLength-2,2);
+                    //relative pathname to child directory w/o "./"
+                    currentPath=currentPath+"/"+newPath[i];
                     i=i+2;
                 }
-                else{
-                    //root directory
-                    currentPath.erase(pathLength-1,2);
-                    i=i+2;
-                }
-                 */
-                currentPath=newPath;
-                i=i+newPathLength;
-            }
-            else if(newPath[i]=='.'&&newPath[i+1]=='/'){
-                //relative pathname to child directory
-                i=i+2;
-            }
-            else{
-                //relative pathname to child directory w/o "./"
-                currentPath=currentPath+"/"+newPath[i];
-                i=i+2;
             }
         }
+        else{
+            //cd command followed by nothing -> go to root directory
+            currentPath="/";
+        }
+        
         
     }
     
@@ -104,7 +109,25 @@ private:
 int main()
 {
     Path path("/a/b/c/d");
-    path.cd("../x");
+    //test cases:
+    //path.cd("../x");  // '/a/b/c/x' -> example test case
+    //path.cd("..");  // '/a/b/c' -> relative pathname to parent directory
+    //path.cd("./x"); // '/a/b/c/d/x' -> relative pathname to child directory
+    //path.cd("x");   // '/a/b/c/d/x' -> relative pathname to child directory
+    //path.cd("/a/b/c"); // '/a/b/c' -> direct pathname to parent
+    //path.cd("../../x"); // '/a/b/x'
+    //path.cd("./x/../../y"); // 'a/b/c/y'
+    //path.cd("x/../../y"); // 'a/b/c/y'
+    //path.cd("../../../.."); // '/'
+    //path.cd("x/..");    // '/a/b/c/d'
+    //path.cd("");    // '/'
+    //path.cd("/");   // '/'
+    //path.cd("~");   // '/'
+    //path.cd(".");   // '/a/b/c/d
+    //path.cd("/x/y/z");  // '/x/y/z/' -> another direct pathname
+    //path.cd("x/y/z  ");   // '/a/b/c/d/x/y/z'
+    path.cd("/a/b/c/../x"); // '/a/b/x'
+    
     std::cout << path.getPath();
 }
 #endif
