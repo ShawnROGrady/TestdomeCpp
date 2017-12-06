5fe27bb174a89148b5702a39d0c714cd0f59afef
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-05 22:51:51 -0600

    Added Problem Statement
    Added the problem statement to the comments at the beginning of the file

--- path.cpp
+++ path.cpp
@@ -5,7 +5,20 @@
 //  Created by Shawn O'Grady on 12/5/17.
 //  Copyright © 2017 Shawn O'Grady. All rights reserved.
 //
-
+/*
+ This code is a practice C++ interview question from testdome.com
+ https://www.testdome.com/for-developers/solve-question/12281
+ 
+ Problem statement: Write a function that provides change directory (cd) function for an abstract file system.
+    -additional notes:
+        -root path is '/'
+ `      -path separator is '\'
+        -parent directory is addressable as ".."
+        -directory names consist only of English alphabet letters (A-Z and a-z)
+        -the function should support both realtive and absolute paths
+        -the function will not be passed any invalid paths
+        -do not use built-in path-related functions
+ */
 #include <stdio.h>
 #include <iostream>
 #include <string>
